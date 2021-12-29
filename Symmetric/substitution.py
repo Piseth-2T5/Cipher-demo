1d36aaab249879dc1b07ba3dd930e742d5880743
2seth <hengsovanpiseth17@gmail.com>
2021-12-29 19:38:21 +0700

    [TO BE CONT]

--- Symmetric/substitution.py
+++ Symmetric/substitution.py
@@ -1,4 +1,14 @@
 pltxt_alp = 'abcdefghijklmnopqrstuvwxyz'
 fstrot13,sndrot13 = pltxt_alp[:14],pltxt_alp[14:]
 
+def encrypt(input="hello world"):
+    cptxt = ""
+    for elem in input:
+        if elem in fstrot13:
+            indx = fstrot13()
+            cptxt += cptxt + fstrot13[indx]
+        elif elem in sndrot13:
+            cptxt += cptxt + sndrot13[indx]
+    print(cptxt)
 
+encrypt()
--- Symmetric/transposition.py
+++ Symmetric/transposition.py
@@ -1,22 +1,24 @@
 input_string = 'this just test string but it is real string'
-key = '693284175'
+key = '647139582'
 fill = 'a'
+
+
 def encrypt(key=key, input=input_string):
-    plainttxt = input.replace(" ","*")
-    positionedtxt, pos_ciphertxt, ciphertxt=[],[],[]
+    plainttxt = input.replace(" ", "*")
+    positionedtxt, pos_ciphertxt, ciphertxt = [], [], []
     key_length = len(key)
-    iteration = len(plainttxt)//key_length + len(plainttxt)%key_length
+    iteration = len(plainttxt)//key_length + len(plainttxt) % key_length
     for j in range(iteration):
-        start,end = key_length * j, key_length * j + key_length
+        start, end = key_length * j, key_length * j + key_length
         positionedtxt.append(plainttxt[start:end])
         if positionedtxt[j] == "":
             del positionedtxt[j]
             break
         if len(positionedtxt[j]) < len(positionedtxt[0]):
             gap = len(positionedtxt[0]) - len(positionedtxt[j])
-            positionedtxt[j]+= fill*gap
+            positionedtxt[j] += fill*gap
 
-    for text in positionedtxt: #012345 632415 thisis
+    for text in positionedtxt:  # 012345 632415 thisis
         text = " " + text
         for i in range(len(key)):
             pos_ciphertxt.insert(i, text[int(key[i])])  #
@@ -26,22 +28,23 @@ def encrypt(key=key, input=input_string):
 
 
 def decypt(key=key, ciphertxt=encrypt()):
-    txt, pltxt, pos_pltxt= [],[],[]
-    itr = len(ciphertxt)//len(key) + len(ciphertxt)%len(key)
+    txt, pltxt, pos_pltxt = [], [], []
+    itr = len(ciphertxt)//len(key) + len(ciphertxt) % len(key)
     for i in range(itr):
-        start, end = len(key)*i , len(key)*i + len(key)
+        start, end = len(key)*i, len(key)*i + len(key)
         txt.append(ciphertxt[start:end])
 
-    for elem in txt: #632415  thisis sihsti 
+    for elem in txt:  # 632415  thisis sihsti
         elem = " " + elem
-        for i in range(1,len(key)+1):
-            pltxt.insert(i,elem[key.index(str(i))+1])
+        for i in range(1, len(key)+1):
+            pltxt.insert(i, elem[key.index(str(i))+1])
         pos_pltxt.append("".join(pltxt))
         del pltxt[:]
         num = len(ciphertxt) - len(input_string)
     result = "".join(pos_pltxt)
-    final_res = result.replace(fill*num,"")
+    final_res = result.replace(fill*num, "")
     return final_res.replace('*', " ")
+
 
 ciphertxt = encrypt()
 plaintxt = decypt()
