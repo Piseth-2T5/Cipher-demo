bacfc6934d5ffb9ccec06ceb1aa8435fddef7caf
Piseth <hengsovanpiseth17@gmail.com>
2022-01-05 12:48:38 +0700

    Add key for more security

--- Cryptography/Symmetric/substitution.py
+++ Cryptography/Symmetric/substitution.py
@@ -1,21 +1,38 @@
 pltxt_alp1, pltxt_alp2= 'abcdefghijklm12865','nopqrstuvwxyz47390'
-input_string = 'Hello world for test string 2021-12-29 Wednesday'
+input_string = 'GLOBAL Hello world for test string 2021-12-29 Thursday'
+key = 7
+
+def shiftOrder(pltxt_alp, key = key):
+    shifted = []
+    indxcounter = 0
+    while True:
+        if indxcounter >= len(pltxt_alp)-key:
+            key, indxcounter = 0, 0
+        shifted.append(pltxt_alp[indxcounter+key])
+        indxcounter+=1
+        if len(shifted) == len(pltxt_alp):
+            return "".join(shifted)
+
+
+
 
 def encrypt(input=input_string):
+    pltxt_alpA = shiftOrder(pltxt_alp1)
+    pltxt_alpB =shiftOrder(pltxt_alp2)
     cptxt = ""
     for char in input:
         if char in pltxt_alp1:
             indx  = pltxt_alp1.find(char)
-            cptxt += pltxt_alp2[indx]
+            cptxt += pltxt_alpB[indx]
         elif char in pltxt_alp2:
             indx = pltxt_alp2.find(char)
-            cptxt += pltxt_alp1[indx]
+            cptxt += pltxt_alpA[indx]
         elif char.upper() in pltxt_alp1.upper():
             indx = pltxt_alp1.upper().find(char.upper())
-            cptxt += pltxt_alp2.upper()[indx]
+            cptxt += pltxt_alpB.upper()[indx]
         elif char.upper() in pltxt_alp2.upper():
             indx = pltxt_alp2.upper().find(char.upper())
-            cptxt += pltxt_alp1.upper()[indx]
+            cptxt += pltxt_alpA.upper()[indx]
         elif char == ' ':
             cptxt += ' '
         else:
@@ -26,18 +43,21 @@ def encrypt(input=input_string):
 
 def decrypt(cptxt = encrypt()):
     pltxt = ""
+    pltxt_alpA = shiftOrder(pltxt_alp1)
+    pltxt_alpB = shiftOrder(pltxt_alp2)
+
     for char in cptxt:
-        if char in pltxt_alp1:
-            indx = pltxt_alp1.find(char)
+        if char in pltxt_alpA:
+            indx = pltxt_alpA.find(char)
             pltxt += pltxt_alp2[indx]
-        elif char in pltxt_alp2:
-            indx = pltxt_alp2.find(char)
+        elif char in pltxt_alpB:
+            indx = pltxt_alpB.find(char)
             pltxt += pltxt_alp1[indx]
-        elif char.upper() in pltxt_alp1.upper():
-            indx = pltxt_alp1.upper().find(char.upper())
+        elif char.upper() in pltxt_alpA.upper():
+            indx = pltxt_alpA.upper().find(char.upper())
             pltxt += pltxt_alp2[indx].upper()
-        elif char.upper() in pltxt_alp2.upper():
-            indx = pltxt_alp2.upper().find(char.upper())
+        elif char.upper() in pltxt_alpB.upper():
+            indx = pltxt_alpB.upper().find(char.upper())
             pltxt += pltxt_alp1[indx].upper()
         elif char == " ":
             pltxt += ' '
@@ -53,3 +73,4 @@ dectxt = decrypt()
 print(f'\n Input string: {input_string} \
     \n\n Encrypted: {enctxt} \
     \n\n Decrypted: {dectxt}\n')
+
